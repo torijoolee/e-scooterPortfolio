3343b6840927418a998d360ff79b7c618af5f097
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-01-17 22:35:11 +0900

    JS: sticky..

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 (() => {
   const imageItems = document.querySelectorAll(".image-item");
   const textElems = document.querySelectorAll(".text");
-  let currentItem = textElems[0];
+  let currentItem = imageItems[0];
 
   for (let i = 0; i < textElems.length; i++) {
-    imageItems[i].dataset.index = i;
     textElems[i].dataset.index = i;
+    imageItems[i].dataset.index = i;
   }
+
+  function activate() {
+    currentItem.classList.add("visible");
+  }
+
+  function inactivate() {
+    currentItem.classList.remove("visible");
+  }
+
   function handleScroll() {
     let text;
     let boundingRect;
@@ -19,12 +28,14 @@
         boundingRect > window.innerHeight * 0.1 &&
         boundingRect < window.innerHeight * 0.9
       ) {
-        currentItem.classList.remove("visible");
+        inactivate();
+
         currentItem = imageItems[text.dataset.index];
-        currentItem.classList.add("visible");
+        activate();
       }
     }
   }
+  activate();
 
   window.addEventListener("scroll", handleScroll);
 })();
