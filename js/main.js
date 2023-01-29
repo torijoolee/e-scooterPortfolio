deef6dcea7a028e2d092515f5e8c77fcdd482ade
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-01-29 14:11:01 +0900

    CSS,JS: ScrolltoTop btn

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@
   const imageItems = document.querySelectorAll(".image-item");
   const textElems = document.querySelectorAll(".text");
   let currentItem = imageItems[0];
+  const scrollTopElem = document.querySelector(".scrollTop");
 
-  const btnElem = document.querySelector(".btn");
+  function handlescrollTop() {
+    const header = document.querySelector("#section0");
+    const headerHight = header.offsetHeight;
+    console.log(headerHight);
+    if (window.scrollY > headerHight / 2) {
+      scrollTopElem.classList.add("visible");
+    } else {
+      scrollTopElem.classList.remove("visible");
+    }
+  }
+
+  window.addEventListener("scroll", handlescrollTop);
 
   for (let i = 0; i < textElems.length; i++) {
     textElems[i].dataset.index = i;
